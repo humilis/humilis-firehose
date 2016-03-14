5feea34fceb141fc95439ee99de7d8a34e958894
German Gomez-Herrero <german@innovativetravel.eu>
2016-03-14 12:46:51 +0100

    Release 0.0.6: fix Pypi docs

--- humilis_firehose/metadata.py
+++ humilis_firehose/metadata.py
@@ -3,7 +3,7 @@
 
 package = "humilis-firehose"
 project = "humilis-firehose"
-version = '0.0.5'
+version = '0.0.6'
 description = ("A humilis plug-in that deploys Firehose delivery streams "
                "Cloudformation")
 authors = ["German Gomez-Herrero"]
